c89bd1ac8f22735a95ccc6acbb37f296ec389c70
Sarah Lagares <smlagares99@gmail.com>
2023-04-23 17:24:05 -0400

    Initial commit

---
+++ src/com/coderscampus/game/GuessingGameApplication.java
@@ -0,0 +1,41 @@
+package com.coderscampus.game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGameApplication {
+
+	public static void main(String[] args) {
+
+		Random random = new Random();
+		int randomNumber = random.nextInt(100);
+
+		System.out.println("Pick a number between 1 and 100");
+
+		Scanner scanner = new Scanner(System.in);
+		Integer userInput = scanner.nextInt();
+
+		while (userInput < 1 || userInput > 100) {
+			System.out.println("Your guess is not between 1 and 100, please try again");
+			userInput = scanner.nextInt();
+		}
+
+		for (int guessCount = 1; guessCount <= 4; guessCount++) {
+
+			if (userInput == randomNumber) {
+				System.out.println("You win!");
+				System.out.println("The number to guess was: " + randomNumber);
+				return;
+			} if (userInput < randomNumber) {
+				System.out.println("Please pick a higher number");
+				userInput = scanner.nextInt();
+			} if (userInput > randomNumber) {
+				System.out.println("Please pick a lower number");
+				userInput = scanner.nextInt();
+			}
+		}
+
+		System.out.println("You lose!");
+		System.out.println("The number to guess was: " + randomNumber);
+	}
+}
